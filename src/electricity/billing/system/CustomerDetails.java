0789a1036a5a8b12a174dd9bf6f45776963c806c
NabajitUtsab <noboutsab@gmail.com>
2024-03-13 16:47:25 +0600

    Finished CustomerDetails By NabajitUtsab Date : 13 March

--- src/electricity/billing/system/CustomerDetails.java
+++ src/electricity/billing/system/CustomerDetails.java
@@ -1,15 +1,19 @@
 package electricity.billing.system;
 
-
+import net.proteanit.sql.DbUtils;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.print.PrinterException;
 import java.sql.ResultSet;
 
-public class CustomerDetails extends JFrame {
+public class CustomerDetails extends JFrame implements ActionListener{
     JLabel searchMeter,searchName;
     Choice searchMeterChoice,searchNameChoice;
     JTable table;
+    JButton searchButton,printButton,closeButton;
     CustomerDetails(){
         super("Customer Details");
 
@@ -63,17 +67,70 @@ public class CustomerDetails extends JFrame {
             Database database =new Database();
             ResultSet resultSet= database.statement.executeQuery("select * from new_customer");
 
-            ///-------- will start from there
+            table.setModel(DbUtils.resultSetToTableModel(resultSet));
 
         }
         catch (Exception e){
             e.printStackTrace();
         }
 
+        JScrollPane scrollPane =new JScrollPane(table);
+        scrollPane.setBounds(0,100,700,500);
+        scrollPane.setBackground(Color.white);
+        add(scrollPane);
+
+
+        searchButton = new JButton("Search");
+        searchButton.setBounds(20,70,80,20);
+        searchButton.setBackground(Color.WHITE);
+        searchButton.setForeground(Color.BLACK);
+        searchButton.addActionListener(this);
+        add(searchButton);
+
+        printButton = new JButton("Print");
+        printButton.setBounds(120,70,80,20);
+        printButton.setBackground(Color.WHITE);
+        printButton.setForeground(Color.BLACK);
+        printButton.addActionListener(this);
+        add(printButton);
+
+        closeButton = new JButton("Close");
+        closeButton.setBounds(550,70,80,20);
+        closeButton.setBackground(Color.WHITE);
+        closeButton.setForeground(Color.BLACK);
+        closeButton.addActionListener(this);
+        add(closeButton);
+
         setVisible(true);
 
     }
     public static void main(String[] args) {
         new CustomerDetails();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource()==searchButton){
+            String query_search = "select * from new_customer where meter_no ='"+searchMeterChoice.getSelectedItem()+"' and name = '"+searchNameChoice.getSelectedItem()+"' ";
+            try{
+                Database database = new Database();
+                ResultSet resultSet = database.statement.executeQuery(query_search);
+                table.setModel(DbUtils.resultSetToTableModel(resultSet));
+
+            }catch(Exception exception){
+                exception.printStackTrace();
+            }
+        }
+        else if(e.getSource()==printButton){
+            try{
+                table.print();
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        }
+        else {
+            setVisible(false);
+        }
+
     }
 }
